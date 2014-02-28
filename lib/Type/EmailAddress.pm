5216c13907e7c9b09cfcb40ef0d76b1003b8dd25
André Walker <andre@andrewalker.net>
2014-02-28 16:57:44 -0300

    Implement EmailAddress type constraint

--- lib/Type/EmailAddress.pm
+++ lib/Type/EmailAddress.pm
@@ -1,12 +1,27 @@
 package Type::EmailAddress;
+# ABSTRACT: type constraints for email addresses
+
 use warnings;
 use strict;
-use Type::Utils -all;
+use Email::Valid ();
+use Type::Utils qw(declare as where inline_as);
 use Types::Standard qw( Str );
 use Type::Library
    -base,
    -declare => qw( EmailAddress );
 
 # VERSION;
+
+declare EmailAddress,
+  as Str,
+  where { Email::Valid->address( -address => $_ ) },
+  inline_as {
+     my ($constraint, $varname) = @_;
+     return sprintf(
+        '%s and Email::Valid->address( -address => %s )',
+        $constraint->parent->inline_check($varname),
+        $varname,
+     );
+  };
 
 1;
